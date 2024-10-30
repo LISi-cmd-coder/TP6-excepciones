279c0fc13f315d9b406fa0b8f6b91fad75ed2870
Lisandro <87555178+LISi-cmd-coder@users.noreply.github.com>
2024-10-30 16:21:22 -0300

    Add files via upload

---
+++ 2.1/CirculosIgualesException.java
@@ -0,0 +1,6 @@
+public class CirculosIgualesException extends Exception {
+    // Constructor que recibe un mensaje descriptivo del error
+    public CirculosIgualesException(String mensaje) {
+        super(mensaje);
+    }
+}
